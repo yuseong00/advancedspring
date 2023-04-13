9ebf05c42204f75920b08593c5a28b1d3a6f1108
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-04-13 23:14:25 +0900

    정보입력

--- src/main/java/hello/advanced/trace/TraceStatus.java
+++ src/main/java/hello/advanced/trace/TraceStatus.java
@@ -1,6 +1,6 @@
 package hello.advanced.trace;
 
-public class TraceStatus {
+public class TraceStatus { // 시작한 로그의 상태 정보를 담는 클래스
 
     private TraceId traceId;    // 트랜잭션 id
     private Long startTimeMs;   // 로그시작 시간

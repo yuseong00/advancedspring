acb1c2690fcbeef0d5a82083fde2ff3f3c4c33fb
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-04-14 22:46:06 +0900

    코드 확인

--- src/main/java/hello/advanced/trace/TraceStatus.java
+++ src/main/java/hello/advanced/trace/TraceStatus.java
@@ -5,7 +5,6 @@ public class TraceStatus { // 시작한 로그의 상태 정보를 담는 클래
     private TraceId traceId;    // 트랜잭션 id
     private Long startTimeMs;   // 로그시작 시간
     private String message;    // 메시지
-
     public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
         this.traceId = traceId;
         this.startTimeMs = startTimeMs;

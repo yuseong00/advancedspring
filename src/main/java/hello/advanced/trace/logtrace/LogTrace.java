b67f652b841eb47fbefcb001131724fdf0690ad8
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-04-15 22:39:31 +0900

    필드 동기화 개발

---
+++ src/main/java/hello/advanced/trace/logtrace/LogTrace.java
@@ -0,0 +1,15 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+
+public interface LogTrace {
+
+    //확작성을 생각하여 LogTrace 인터페이스에는 최소한의 기능인 메소드를 정의했다.
+
+    TraceStatus begin(String message);
+
+    void end(TraceStatus status);
+
+    void exception(TraceStatus status, Exception e);
+
+}
